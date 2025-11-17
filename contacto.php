bb7cb5ce9c5096f04cd0e6e54f11dd90b5a7dab8
FlopyP <flopypaez@hotmail.com>
2025-11-17 16:59:48 -0300

    version final?

--- contacto.php
+++ contacto.php
@@ -9,9 +9,9 @@
 //Preparamos el mensaje de contacto
 $cabeceras = "From: $email\n" //La persona que envia el correo
  . "Reply-To: $email\n";
-$asunto = "CONSULTA LANDING CAM SEGUROS"; //asunto aparecera en la bandeja del servidor de correo
-$email_to = "info@camseguros.com.ar"; //cambiar por tu email
-$contenido = "$nombre ha enviado un mensaje desde la web www.camseguros.com.ar/\n"
+$asunto = "CONSULTA desde Pagina"; //asunto aparecera en la bandeja del servidor de correo
+$email_to = "fpaez@microcomputers.com.ar"; //cambiar por tu email
+$contenido = "$nombre ha enviado un mensaje desde la web williamscompanysa.com/\n"
 . "\n"
 . "NOMBRE: $nombre\n"
 . "EMAIL: $email\n"
@@ -23,10 +23,10 @@ $contenido = "$nombre ha enviado un mensaje desde la web www.camseguros.com.ar/\
 if (@mail($email_to, $asunto ,$contenido ,$cabeceras )) {
 
 //Si el mensaje se envía muestra una confirmación
-header("Location: https://www.camseguros.com.ar/contacto-gracias.html");
+header("Location: https://v1.williamscompanysa.com /contacto-gracias.html");
 
 }else{
 //Si el mensaje no se envía muestra el mensaje de error
-header("Location: https://www.camseguros.com.ar");
+header("Location: https://v1.williamscompanysa.com ");
 }
 ?>
